317a86a97a08bd96739bd31e86a9cce9fdfdc43b
bohemian9485 <tubianosaric26@gmail.com>
2022-08-18 12:30:12 +0800

    Add project files.

---
+++ FrmMain.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmMain))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TxtMinutes = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TxtFractions = New System.Windows.Forms.TextBox()
+        Me.BtnExit = New System.Windows.Forms.Button()
+        Me.ChkTopMost = New System.Windows.Forms.CheckBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Minutes"
+        '
+        'TxtMinutes
+        '
+        Me.TxtMinutes.Location = New System.Drawing.Point(147, 12)
+        Me.TxtMinutes.Name = "TxtMinutes"
+        Me.TxtMinutes.Size = New System.Drawing.Size(182, 24)
+        Me.TxtMinutes.TabIndex = 1
+        Me.TxtMinutes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.ToolTip1.SetToolTip(Me.TxtMinutes, "Minutes to convert (maximum 3,840 minutes)")
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(129, 18)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "&Fractions of a Day"
+        '
+        'TxtFractions
+        '
+        Me.TxtFractions.Location = New System.Drawing.Point(147, 42)
+        Me.TxtFractions.Name = "TxtFractions"
+        Me.TxtFractions.ReadOnly = True
+        Me.TxtFractions.Size = New System.Drawing.Size(182, 24)
+        Me.TxtFractions.TabIndex = 3
+        Me.TxtFractions.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.ToolTip1.SetToolTip(Me.TxtFractions, "Fractions of a day")
+        '
+        'BtnExit
+        '
+        Me.BtnExit.Location = New System.Drawing.Point(254, 72)
+        Me.BtnExit.Name = "BtnExit"
+        Me.BtnExit.Size = New System.Drawing.Size(75, 27)
+        Me.BtnExit.TabIndex = 5
+        Me.BtnExit.Text = "E&xit"
+        Me.ToolTip1.SetToolTip(Me.BtnExit, "Exit application")
+        Me.BtnExit.UseVisualStyleBackColor = True
+        '
+        'ChkTopMost
+        '
+        Me.ChkTopMost.AutoSize = True
+        Me.ChkTopMost.Location = New System.Drawing.Point(12, 74)
+        Me.ChkTopMost.Name = "ChkTopMost"
+        Me.ChkTopMost.Size = New System.Drawing.Size(131, 22)
+        Me.ChkTopMost.TabIndex = 4
+        Me.ChkTopMost.Text = "&Top Most Form"
+        Me.ToolTip1.SetToolTip(Me.ChkTopMost, "Set this window on top of other windows")
+        Me.ChkTopMost.UseVisualStyleBackColor = True
+        '
+        'ToolTip1
+        '
+        Me.ToolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info
+        Me.ToolTip1.ToolTipTitle = "Conversion Calculator"
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(341, 111)
+        Me.Controls.Add(Me.ChkTopMost)
+        Me.Controls.Add(Me.BtnExit)
+        Me.Controls.Add(Me.TxtFractions)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TxtMinutes)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.Name = "FrmMain"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Conversion Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TxtMinutes As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TxtFractions As TextBox
+    Friend WithEvents BtnExit As Button
+    Friend WithEvents ChkTopMost As CheckBox
+    Friend WithEvents ToolTip1 As ToolTip
+End Class
---
+++ FrmMain.vb
@@ -0,0 +1,63 @@
+﻿Public Class FrmMain
+    Private conversionWrapper As New FractionsOfADay.ClsConversion
+
+    Private Const MAX_MINUTES As Integer = 3840
+    Private Const WORK_MINUTES As Integer = 480
+    Private Const MINUTES_IN_HOUR As Integer = 60
+
+    Private Sub FrmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Text += " v" & My.Application.Info.Version.ToString
+        conversionWrapper.GetClassSettings()
+        TopMost = ChkTopMost.Checked
+    End Sub
+
+    Private Sub FrmMain_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        conversionWrapper = Nothing
+    End Sub
+
+    Private Sub TxtMinutes_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TxtMinutes.KeyPress
+        ' Limit user input to numeric characters
+        '97 - 122 = Ascii codes for simple letters
+        '65 - 90  = Ascii codes for capital letters
+        '48 - 57  = Ascii codes for numbers
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub TxtMinutes_TextChanged(sender As Object, e As EventArgs) Handles TxtMinutes.TextChanged
+        Dim fractionsOfADay As Decimal
+        If TxtMinutes.Text <> vbNullString Then
+            Dim minutesEntered As Integer = CInt(TxtMinutes.Text)
+            If minutesEntered <= MAX_MINUTES Then
+                ' Checks if minutes entered is greater than one hour
+                Dim convertedToHours As Integer = Int(minutesEntered / WORK_MINUTES)
+                If convertedToHours > 1 Then
+                    Dim sixtyMinuteFraction As Decimal = conversionWrapper.FractionsOfADay(MINUTES_IN_HOUR)
+                    fractionsOfADay = sixtyMinuteFraction * convertedToHours
+                    Dim remainder As Integer = minutesEntered Mod WORK_MINUTES
+                    fractionsOfADay += conversionWrapper.FractionsOfADay(remainder)
+                Else
+                    fractionsOfADay = conversionWrapper.FractionsOfADay(minutesEntered)
+                End If
+                TxtFractions.Text = Format(fractionsOfADay, "0.000")
+            Else
+                TxtFractions.Text = vbNullString
+                TxtMinutes.Focus()
+                Dim unUsed = MessageBox.Show("Minutes entered is greater than eight (8) hours (3,840 minutes).", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Else
+            TxtFractions.Text = vbNullString
+        End If
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+        Close()
+    End Sub
+
+    Private Sub ChkTopMost_CheckedChanged(sender As Object, e As EventArgs) Handles ChkTopMost.CheckedChanged
+        TopMost = ChkTopMost.Checked
+    End Sub
+End Class
